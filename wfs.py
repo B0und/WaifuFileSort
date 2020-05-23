d62cef7de6b649441887435f792311c8a9c5b03f
Vlad <f1arenova1@gmail.com>
2020-05-23 13:49:01 +0300

    want to change send cb

--- wfs.py
+++ wfs.py
@@ -1,5 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QShortcut, QFileSystemModel, QAbstractItemView, QFileDialog
+from PyQt5.QtWidgets import (
+    QShortcut,
+    QFileSystemModel,
+    QAbstractItemView,
+    QFileDialog,
+    QDialog,
+)
 from PyQt5.QtGui import (
     QColor,
     QPalette,
@@ -14,6 +20,9 @@ import pathlib
 from pprint import pprint
 import shutil
 from send2trash import send2trash
+from functools import partial
+import json
+
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -23,12 +32,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle("Weeb File Sort")
-        # app_icon = QtGui.QIcon()
-        # app_icon.addFile("ui_compile/icons/icon.png", QtCore.QSize(256, 256))
-        # self.setWindowIcon(app_icon)
+        app_icon = QtGui.QIcon()
+        app_icon.addFile("waifu_sort.png", QtCore.QSize(256, 256))
+        self.setWindowIcon(app_icon)
 
         self.path_hotkey_dict = {}
         self.hotkey_path_dict = {}
+        self._shortcut_list = []
+        self.undo_list = []
 
         self.model = QFileSystemModel()
         self.model.setFilter(QDir.Files)
@@ -37,6 +48,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.listView.setModel(self.model)
         # self.ui.listView.setRootIndex(self.model.index(p))
         self.ui.listView.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.ui.tableWidget.setSelectionMode(
+            QtWidgets.QAbstractItemView.SingleSelection
+        )
 
         self.ui.listView.selectionModel().selectionChanged.connect(
             self.updateImageLabel
@@ -47,7 +61,82 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.delShortcut = QShortcut(QKeySequence("Delete"), self)
         self.delShortcut.activated.connect(self.delete_cb)
+
+        self.undoShortcut = QShortcut(QKeySequence("Ctrl+Z"), self)
+        self.undoShortcut.activated.connect(self.undo_cb)
+
         self.ui.removeDest.clicked.connect(self.remove_dest)
+        self.ui.actionAbout.triggered.connect(self.showAboutDialog)
+        self.ui.actionSave_Preset.triggered.connect(self.save_preset_cb)
+        self.ui.actionLoad_Preset.triggered.connect(self.load_preset_cb)
+
+    def 
+
+    def undo_cb(self):
+        print("undoing")
+
+    def load_preset_cb(self):
+        dialog = QFileDialog()
+        dialog.setFilter(dialog.filter() | QtCore.QDir.Hidden)
+        dialog.setDefaultSuffix("json")
+        dialog.setAcceptMode(QFileDialog.AcceptOpen)
+        dialog.setNameFilters(["JSON (*.json)"])
+        if dialog.exec_() == QDialog.Accepted:
+            preset_path = dialog.selectedFiles()[0]
+            self.path_hotkey_dict = load_json(preset_path)
+            self.path_hotkey_dict = {
+                k: v for k, v in self.path_hotkey_dict.items() if v is not None
+            }
+            print("loaded dict: ", self.path_hotkey_dict)
+            self.hotkey_path_dict = {
+                value: key for key, value in self.path_hotkey_dict.items()
+            }
+            self.restore_table_from_dict()
+        else:
+            print("Cancelled")
+
+    def save_preset_cb(self):
+        dialog = QFileDialog()
+        dialog.setFilter(dialog.filter() | QtCore.QDir.Hidden)
+        dialog.setDefaultSuffix("json")
+        dialog.setAcceptMode(QFileDialog.AcceptSave)
+        dialog.setNameFilters(["JSON (*.json)"])
+        if dialog.exec_() == QDialog.Accepted:
+            preset_path = dialog.selectedFiles()[0]
+            save_json(self.path_hotkey_dict, preset_path)
+            QtWidgets.QMessageBox.information(
+                self, "Saved", f"Saved hotkey preset: {preset_path}"
+            )
+        else:
+            print("Cancelled")
+
+    def showAboutDialog(self):
+        text = (
+            "<center>"
+            "<h1>Waifu File Sort</h1>"
+            "&#8291;"
+            "</center>"
+            "<p>Version 1.1.0<br/>"
+        )
+        QtWidgets.QMessageBox.about(self, "About Waifu File Sort", text)
+
+    def restore_table_from_dict(self):
+        self.clearTableWidget()
+        row_counter = 0
+
+        for shortcut in self._shortcut_list:
+            shortcut.setEnabled(False)
+
+        self._shortcut_list = []
+
+        for path, hotkey in self.path_hotkey_dict.items():
+            path = pathlib.Path(path)
+            self.add_dest_to_table(dest_path=path.name, hotkey=hotkey)
+            self.ui.tableWidget.item(row_counter, 0).setToolTip(str(path))
+            shortcut = QShortcut(QKeySequence(hotkey), self)
+            shortcut.activated.connect(lambda: self.send_from_hotkey(path))
+            self._shortcut_list.append(shortcut)
+            row_counter += 1
 
     def clearTableWidget(self):
         self.ui.tableWidget.clearContents()
@@ -57,15 +146,35 @@ class MainWindow(QtWidgets.QMainWindow):
         # get selected row or return
         # delete info from both dicts
         # reconstruct table widget from dict
-        current_row = self.ui.tableWidget.currentRow()
-        print(current_row)
-        if current_row == -1:
-            return
+        # items = self.ui.tableWidget.selectedItems()
 
-        dest_path = self.ui.tableWidget.item(current_row, 0).toolTip()
+        current_row = self.ui.tableWidget.currentRow()
+        print(f"{current_row=}")
+        try:
+            dest_path = self.ui.tableWidget.item(current_row, 0).toolTip()
+        except AttributeError:
+            return
         hotkey = self.ui.tableWidget.cellWidget(current_row, 2).text()
-        del self.path_hotkey_dict[dest_path]
-        del self.hotkey_path_dict[hotkey]
+        print("deleting hotkey: ", hotkey)
+        self.delete_hotkey(hotkey)
+        try:
+            del self.path_hotkey_dict[dest_path]
+        except KeyError:
+            pass
+        try:
+            del self.hotkey_path_dict[hotkey]
+        except KeyError:
+            pass
+
+        self.restore_table_from_dict()
+
+    def delete_hotkey(self, name):
+        for shortcut in self._shortcut_list:
+            key_name = shortcut.key().toString()
+            print("k-name: ", key_name)
+            if key_name == name.upper():
+                print("DELETED hotkey: ", name)
+                shortcut.setEnabled(False)
 
     def delete_cb(self):
         ind = self.ui.listView.currentIndex()
@@ -73,7 +182,7 @@ class MainWindow(QtWidgets.QMainWindow):
         p = pathlib.Path(file_path)
         send2trash(str(p))
 
-    def add_dest_to_table(self):
+    def add_dest_to_table(self, dest_path=None, hotkey=None):
         self.ui.tableWidget.setEditTriggers(
             self.ui.tableWidget.NoEditTriggers
         )  # disable editing and sorting
@@ -81,11 +190,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
         row_counter = self.ui.tableWidget.rowCount()
         self.ui.tableWidget.insertRow(row_counter)
-
         ########################################################
         # add path label
         dest_path = QtWidgets.QTableWidgetItem(
-            "Press browse to specify destination directory"
+            dest_path or "Press browse to specify destination directory"
         )
         self.ui.tableWidget.setItem(row_counter, 0, dest_path)
         ########################################################
@@ -99,6 +207,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # add hotkey line edit
         hotkey_line = QtWidgets.QLineEdit()
         hotkey_line.setPlaceholderText("Add hotkey")
+        hotkey_line.setText(hotkey)
         hotkey_line.setMaxLength(1)
 
         hotkey_line.textChanged.connect(
@@ -124,6 +233,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         if dest_path.is_dir() and str(dest_path) != ".":
             shutil.move(pic_path, str(dest_path))
+            self.undo_list.append((pic_path, str(dest_path)))
         else:
             # notify user
             QtWidgets.QMessageBox.warning(
@@ -136,7 +246,10 @@ class MainWindow(QtWidgets.QMainWindow):
         dest_path = pathlib.Path(dest_path)
         # print(dest_path)
         if dest_path.is_dir() and str(dest_path) != ".":
-            shutil.move(pic_path, str(dest_path))
+            try:
+                shutil.move(pic_path, str(dest_path))
+            except shutil.Error:
+                QtWidgets.QMessageBox.warning(self, "Warning", "File already exists")
         else:
             # notify user
             QtWidgets.QMessageBox.warning(
@@ -154,11 +267,18 @@ class MainWindow(QtWidgets.QMainWindow):
             hotkey_line.clearFocus()
             return
 
+        # check if hotkey line edit is empty and delete hotkey
+        if len(hotkey) == 0:
+            hotkey_to_del = self.path_hotkey_dict[path]
+            self.delete_hotkey(hotkey_to_del)
+
         self.path_hotkey_dict[path] = hotkey
         self.hotkey_path_dict = {
             value: key for key, value in self.path_hotkey_dict.items()
         }
         shortcut = QShortcut(QKeySequence(hotkey), self)
+        # self._shortcut_list.append(shortcut.key().toString())
+        self._shortcut_list.append(shortcut)
         dest_path = self.hotkey_path_dict[hotkey]
         shortcut.activated.connect(lambda: self.send_from_hotkey(dest_path))
         if len(hotkey) > 0:
@@ -195,6 +315,17 @@ class MainWindow(QtWidgets.QMainWindow):
         )
         self.ui.imageLabel.setPixmap(pixmap)
         self.ui.imageLabel.setAlignment(QtCore.Qt.AlignCenter)
+
+
+def save_json(data, resultNameAndExt):
+    with open(resultNameAndExt, "w") as fp:
+        json.dump(data, fp)
+
+
+def load_json(inputNameAndExt):
+    with open(inputNameAndExt, "r") as fp:
+        data = json.load(fp)
+    return data
 
 
 if __name__ == "__main__":
